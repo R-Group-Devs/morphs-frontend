0067226606c3853267087a04f76505b9fcfef33e
Zak Angelle <zaknotzak@gmail.com>
2022-02-06 20:58:29 -0600

    fix useShouldAutoConnect hook bug

--- src/hooks/useShouldAutoConnect.ts
+++ src/hooks/useShouldAutoConnect.ts
@@ -6,8 +6,8 @@ export default () => {
   const connectedWallet = localStorage.getItem('wagmi.wallet');
   const shimDisconnect = localStorage.getItem('wagmi.shimDisconnect');
   const shouldAutoConnect =
-    (connectedWallet === `"${wallet.connectors[0].name}"` && shimDisconnect) ||
-    (connectedWallet !== `"${wallet.connectors[0].name}"` && connectedWallet !== undefined);
+    shimDisconnect ||
+    (connectedWallet !== `"${wallet.connectors[0].name}"` && connectedWallet !== null);
 
   return shouldAutoConnect;
 };
