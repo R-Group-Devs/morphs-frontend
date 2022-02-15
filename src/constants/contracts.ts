01d6b2d0a9afa2d309710d5a75586b997f2b282d
Zak Angelle <zaknotzak@gmail.com>
2022-02-15 14:47:50 -0600

    update engine address

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -2,7 +2,7 @@ import { NETWORKS } from './networks';
 
 export const PLAYGROUNDS_GENESIS_ENGINE_CONTRACT_ADDRESSES = {
   // TODO: add ethereum contract
-  [NETWORKS.rinkeby]: '0x1fac6c852cc8b092ac0a5cf4a7f564e383ba861e',
+  [NETWORKS.rinkeby]: '0x6a4ec56c34c3f8bbcc0510add4432373887ccc1a',
 } as const;
 
 export const MORPHS_NFT_CONTRACT_ADDRESSES = {
