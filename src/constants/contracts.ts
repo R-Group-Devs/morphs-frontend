78c1277f52d4902df281005b21b52be9b257d3f6
Zak Angelle <zaknotzak@gmail.com>
2022-02-16 12:49:13 -0600

    add mainnet support (#2)

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,11 +1,11 @@
 import { NETWORKS } from './networks';
 
 export const PLAYGROUNDS_GENESIS_ENGINE_CONTRACT_ADDRESSES = {
-  // TODO: add ethereum contract
+  [NETWORKS.ethereum]: '0xd23c25eb1bad8b1de60cbd313c09209055fd74c0',
   [NETWORKS.rinkeby]: '0xef817cb5ce513be10ce892fb291ac2dec2c1cb40',
 } as const;
 
 export const MORPHS_NFT_CONTRACT_ADDRESSES = {
-  // TODO: add ethereum contract
+  [NETWORKS.ethereum]: '0x480894ceedc8ff63b6db624568f666e634dc8623',
   [NETWORKS.rinkeby]: '0x7b586e2ccb67aad5b800dc8f3ed6545ba6bffd7b',
 } as const;
