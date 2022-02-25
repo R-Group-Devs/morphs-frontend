bf20d2294e6a71959c39dbda14c62e03013e04b4
Zak Angelle <zaknotzak@gmail.com>
2022-02-25 10:18:31 -0600

    update contract addresses (#4)

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,11 +1,11 @@
 import { NETWORKS } from './networks';
 
 export const PLAYGROUNDS_GENESIS_ENGINE_CONTRACT_ADDRESSES = {
-  [NETWORKS.ethereum]: '0xd23c25eb1bad8b1de60cbd313c09209055fd74c0',
-  [NETWORKS.rinkeby]: '0xef817cb5ce513be10ce892fb291ac2dec2c1cb40',
+  [NETWORKS.ethereum]: '0x4993838aE8ED82caf02Cc27D820d92333eD04d0F',
+  [NETWORKS.rinkeby]: '0x3d28Aeea88f70c4dDc370acC3260ACeF040e9E38',
 } as const;
 
 export const MORPHS_NFT_CONTRACT_ADDRESSES = {
   [NETWORKS.ethereum]: '0x480894ceedc8ff63b6db624568f666e634dc8623',
-  [NETWORKS.rinkeby]: '0x7b586e2ccb67aad5b800dc8f3ed6545ba6bffd7b',
+  [NETWORKS.rinkeby]: '0xa857abd882d4f4a5f2e2a7e23c2ab5c34637012a',
 } as const;
