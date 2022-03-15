b4f8c132393171951cf57217b7c66ab0e75b3e6c
Brandon Valosek <bvalosek@gmail.com>
2022-03-14 22:38:24 -0500

    update search-test page reference

--- src/pages/SearchTestPage.tsx
+++ src/pages/SearchTestPage.tsx
@@ -1,8 +1,29 @@
-import { useRef, useState } from "react";
+import { FunctionComponent, useRef, useState } from "react";
 import { useQuery } from "react-query"
 import { Link } from "react-router-dom";
 import useChainId from "../hooks/useChainId";
-import { searchMorphs } from "../lib/search";
+import { getLoaders } from "../lib/dataloaders";
+import { searchMorphsV2 } from "../lib/search-v2";
+
+const Address: FunctionComponent<{ address: string }> = ({ address }) => {
+  const { ensName } = getLoaders(1); // ens always on eth
+  const query = useQuery(['resolve ens', address], () => ensName.load(address));
+  if (query.isLoading || !query.data) {
+    return <>{address}</>
+  }
+
+  return <>{query.data}</>
+}
+
+const MorphItem: FunctionComponent<{ tokenId: string }> = ({ tokenId }) => {
+  const { tokenData } = getLoaders();
+  const query = useQuery(['get morph', tokenId], () => tokenData.load(tokenId));
+  if (query.isLoading || !query.data) {
+    return null;
+  }
+
+  return <>#{query.data.tokenId} {query.data.name}, owner=<Address address={query.data.owner} /></>
+};
 
 export default () => {
   const chainId = useChainId();
@@ -10,10 +31,16 @@ export default () => {
   const nextCursor = useRef<string | undefined>(undefined);
 
   const query = useQuery(['search', chainId, cursor], async () => {
-    // w/o any filter arguments, search the entire catalog
-    // const resp = await searchMorphs({ cursor });
-    const resp = await searchMorphs({ sigils: ['Reaper', 'REAPER'], cursor });
-    console.log(resp);
+    const resp = await searchMorphsV2({
+      cursor,
+      limit: 20,
+      // filter: { type: 'sigil', sigils: ['Reaper', 'REAPER'] }
+      // filter: { type: 'owner', address: '0x303EeFeDeE1bA8e5d507a55465d946B2fea18583' }
+      // filter: { type: 'affinity', affinity: 'mythical' }
+      // filter: { type: 'has sigil' }
+      // filter: { type: 'signature', signature: BigNumber.from(420) }
+      // filter: { type: 'era', era: 'genesis II' }
+    });
     return resp;
   });
 
@@ -23,14 +50,17 @@ export default () => {
     return null;
   }
 
+  const tokenIds = query.data.tokenIds;
+
   return <div>
-    <button onClick={() => setCursor(nextCursor.current)}>next</button>
+    <button onClick={() => setCursor(nextCursor.current)}>{tokenIds.length ? 'next' : 'restart' }</button>
     <div>
-      {query.data.morphs.map(morph => <div>
-        <Link to={`/token/${morph.tokenId}`}>
-          {morph.tokenId} - {morph.name}
+      {tokenIds.map(tokenId => <div>
+        <Link to={`/token/${tokenId}`}>
+          <MorphItem tokenId={tokenId} />
         </Link>
       </div>)}
+      {tokenIds.length === 0 && <p>(none)</p>}
     </div>
   </div>
 
